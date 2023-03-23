fc1b80a8fb488d12cbee6ddcb188e74691da3b8c
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2023-03-22 22:17:27 -0400

    Feat: complete Button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,6 @@ import React from "react";
 import "components/Button.scss";
 import classNames from "classnames";
 
-
 export default function Button(props) {
   const buttonClass = classNames("button", {
     "button--confirm": props.confirm,
