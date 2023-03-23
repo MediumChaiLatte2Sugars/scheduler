7ef37da55aaaeefd27e7fb5bab6d734c3c72fee5
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2023-03-22 22:11:47 -0400

    Refactor: extract classNames into variable

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,8 +5,18 @@ import classNames from "classnames";
 
 
 export default function Button(props) {
+  const buttonClass = classNames("button", {
+    "button--confirm": props.confirm,
+    "button--danger": props.danger
+  });
 
-  return <button onClick={props.onClick} className={classNames("button", { 'button--confirm': props.confirm}, { 'button--danger': props.danger})} disabled={props.disabled}>
-    {props.children}
-  </button>;
+  return (
+    <button
+      className={buttonClass}
+      onClick={props.onClick}
+      disabled={props.disabled}
+    >
+      {props.children}
+    </button>
+  );
 }
