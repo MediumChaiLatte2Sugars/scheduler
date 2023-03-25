124d4b5591da70e6587ae3e2a2b4e1969191a473
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2023-03-25 17:00:32 -0400

    Refactor: consolidate states

--- src/components/Application.js
+++ src/components/Application.js
@@ -48,8 +48,14 @@ const appointments = {
 
 export default function Application(props) {
 
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+  })
+
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({ ...prev, days }));
 
   useEffect(() => {
     axios.get('/api/days')
@@ -59,7 +65,7 @@ export default function Application(props) {
       })
       .catch(err => { console.log(err); });
   }, []);
-
+  
   return (
     <main className="layout">
       <section className="sidebar">
@@ -70,8 +76,8 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu"><DayList
-          days={days}
-          value={day}
+          days={state.days}
+          value={state.day}
           onChange={setDay}
         /></nav>
         <img
@@ -82,7 +88,7 @@ export default function Application(props) {
       </section>
       <section className="schedule">
         {Object.values(appointments).map(appointment => <Appointment key={appointment.id} {...appointment} />)}
-        // Additonal Appointment with key of "last" to allow styling rules to apply to last item
+        {/* Additonal Appointment with key of "last" to allow styling rules to apply to last item */}
         <Appointment key="last" time="5pm" />
       </section>
     </main>
