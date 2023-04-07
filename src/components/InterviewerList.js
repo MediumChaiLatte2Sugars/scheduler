df9510bf0866ee3e3ab2dcbdb9d703d8d3f88955
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2023-04-07 13:14:54 -0400

    Refactor: add key prop to component array

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -5,7 +5,7 @@ import "components/InterviewerList.scss";
 
 export default function InterviewerList(props) {
 
-  const interviewerListItemArray = props.interviewers.map(interviewer => <InterviewerListItem id={interviewer.id} name={interviewer.name} avatar={interviewer.avatar} selected={props.value === interviewer.id} setInterviewer={props.setInterviewer}/>);
+  const interviewerListItemArray = props.interviewers.map(interviewer => <InterviewerListItem key={interviewer.id} id={interviewer.id} name={interviewer.name} avatar={interviewer.avatar} selected={props.value === interviewer.id} setInterviewer={props.setInterviewer}/>);
 
   return <section className="interviewers">
     <h4 className="interviewers__header text--light">Interviewer</h4>
