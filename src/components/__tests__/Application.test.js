1a3ebefbf04a572bcdb1668f3d6a18102b3a5f5b
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2023-04-17 19:04:12 -0400

    Test: implement happy mount, load, edit

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,6 +1,7 @@
 import React from "react";
+import axios from "axios";
 
-import { render, cleanup, waitForElement, fireEvent, getByText, prettyDOM, getAllByTestId, getByAltText, getByPlaceholderText, queryByText, getAllByAltText } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent, getByText, prettyDOM, getAllByTestId, getByAltText, getByPlaceholderText, queryByText, getAllByAltText, getByDisplayValue } from "@testing-library/react";
 
 import Application from "components/Application";
 
@@ -118,5 +119,59 @@ describe("Application", () => {
 
     const noSpotsRemainingMsg = queryByText(day, /2 spots remaining/i);
     expect(noSpotsRemainingMsg).toBeInTheDocument();
+  });
+
+  it("loads data, edits an interview and keeps the spots remaining for Monday the same", async () => {
+    // 1. Render the Application.
+    const { container, debug } = render(<Application />);
+
+    // 2. Wait until the text "Archie Cohen" is displayed.
+    try {
+      await waitForElement(() => getByText(container, "Archie Cohen"));
+    } catch (err) {
+      console.log(err);
+    }
+
+    const appointments = getAllByTestId(container, "appointment");
+    const appointment = appointments[1];
+
+    // 3. Click the "Edit" button on the booked appointment.
+    const editButton = getByAltText(appointment, "Edit");
+    fireEvent.click(editButton);
+    
+    // 4. Check that the editing form is shown with the correct values for student and interviewer.
+    const inputForm = getByPlaceholderText(appointment, /Enter Student Name/i);
+    expect(inputForm).toHaveValue("Archie Cohen");
+    
+    // 5. Enter the edited student's name as a value in the element with the value of "Archie Cohen"
+    fireEvent.change(inputForm, {
+      target: { value: "Lydia Miller-Jones" }
+    });
+    
+    // 6. Click the "Save" button on the appointment
+    const saveButton = getByText(appointment, "Save");
+    fireEvent.click(saveButton);
+    
+    // 7. Check that the element with the text "Saving" is displayed.
+    const savingIndicator = getByText(appointment, "Saving");
+    expect(savingIndicator).toBeInTheDocument();
+    
+    // 8. Wait until the text of the edited student name is displayed.
+    try {
+      await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
+    } catch (err) {
+      console.log(err);
+    }
+
+    // 9. Check that the DayListItem with the text "Monday" also has the text "1 spot remaining".
+    const day = getAllByTestId(container, "day")
+      .find(day => queryByText(day, "Monday"));
+
+    const oneSpotRemainingMsg = queryByText(day, /1 spot remaining/i);
+    expect(oneSpotRemainingMsg).toBeInTheDocument();
+  });
+
+  it("shows the save error when failing to save an appointment", () => {
+    axios.put.mockRejectedValueOnce();
   });
 });
