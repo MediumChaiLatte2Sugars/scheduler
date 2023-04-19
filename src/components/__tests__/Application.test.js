5ecd45e98a29fd476bfff31813e014e85e026fab
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2023-04-18 22:47:35 -0400

    Test: implement mount, load, error book

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -138,24 +138,24 @@ describe("Application", () => {
     // 3. Click the "Edit" button on the booked appointment.
     const editButton = getByAltText(appointment, "Edit");
     fireEvent.click(editButton);
-    
+
     // 4. Check that the editing form is shown with the correct values for student and interviewer.
     const inputForm = getByPlaceholderText(appointment, /Enter Student Name/i);
     expect(inputForm).toHaveValue("Archie Cohen");
-    
+
     // 5. Enter the edited student's name as a value in the element with the value of "Archie Cohen"
     fireEvent.change(inputForm, {
       target: { value: "Lydia Miller-Jones" }
     });
-    
+
     // 6. Click the "Save" button on the appointment
     const saveButton = getByText(appointment, "Save");
     fireEvent.click(saveButton);
-    
+
     // 7. Check that the element with the text "Saving" is displayed.
     const savingIndicator = getByText(appointment, "Saving");
     expect(savingIndicator).toBeInTheDocument();
-    
+
     // 8. Wait until the text of the edited student name is displayed.
     try {
       await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
@@ -171,7 +171,51 @@ describe("Application", () => {
     expect(oneSpotRemainingMsg).toBeInTheDocument();
   });
 
-  it("shows the save error when failing to save an appointment", () => {
-    axios.put.mockRejectedValueOnce();
+  it("shows the save error when failing to save an appointment", async () => {
+    axios.put.mockRejectedValueOnce(new Error("I couldn't do it"));
+    // 1. Render the Application.
+    const { container, debug } = render(<Application />);
+
+    // 2. Wait until the text "Archie Cohen" is displayed.
+    try {
+      await waitForElement(() => getByText(container, "Archie Cohen"));
+    } catch (err) {
+      console.log(err);
+    }
+
+    const appointments = getAllByTestId(container, "appointment");
+    const appointment = appointments[0];
+
+    // 3. Click the "Add" button on the first empty appointment.
+    const addButton = getByAltText(appointment, "Add");
+    fireEvent.click(addButton);
+
+    // 4. Enter the name "Lydia Miller-Jones" into the input with the placeholder "Enter Student Name".
+    const inputField = getByPlaceholderText(appointment, "Enter Student Name");
+    fireEvent.change(inputField, {
+      target: { value: "Lydia Miller-Jones" }
+    });
+
+    // 5. Click the first interviewer in the list.
+    const firstInterviewerButton = getByAltText(appointment, "Sylvia Palmer");
+    fireEvent.click(firstInterviewerButton);
+
+    // 6. Click the "Save" button on that same appointment.
+    const saveButton = getByText(appointment, "Save");
+    fireEvent.click(saveButton);
+    // axios.put.mockRejectedValueOnce();
+
+    // 7. Check that the element with the text "Saving" is displayed.
+    const savingIndicator = getByText(appointment, "Saving");
+    expect(savingIndicator).toBeInTheDocument();
+
+    // 8. Check that the error element is displayed
+    await waitForElement(() => getByText(appointment, "Error"));
+    expect(getByText(appointment, "Error")).toBeInTheDocument();
+
+  });
+
+  it("shows the delete error when failing to delete an existing appointment", async () => {
+    axios.delete.mockRejectedValueOnce();
   });
 });
