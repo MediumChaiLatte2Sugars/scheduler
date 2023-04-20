72fb74b750c4bc66e92c641d409fe0611a7217a0
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2023-04-20 15:56:36 -0400

    Test: E2E happy interview delete

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -5,7 +5,7 @@ describe("Appointments", () => {
     cy.contains("Monday");
   });
 
-  it("should book an interview", () => {
+  xit("should book an interview", () => {
     // 1. Click the Add button on the open appointment
     cy.get("[alt=Add]")
       .first()
@@ -26,7 +26,7 @@ describe("Appointments", () => {
     cy.contains(".appointment__card--show", "Tori Malcolm");
   });
 
-  it("should edit an interview", () => {
+  xit("should edit an interview", () => {
     // 1. CLick the Edit button
     cy.get(".appointment__actions-button")
       .first()
@@ -37,13 +37,37 @@ describe("Appointments", () => {
       .type("Lydia Miller-Jones", { delay: 60 })
       .get("[alt='Tori Malcolm'")
       .click();
-    
+
     // 3. Click the save button
     cy.contains("Save")
       .click();
-    
+
     // 4. Verify the changed appointment is seen
     cy.contains(".appointment__card--show", "Lydia Miller-Jones", "Tori Malcolm");
+
+  });
+
+  it("should cancel an interview", () => {
+    // 1. CLick the Delete button
+    cy.get(".appointment__actions-button")
+      .eq(1)
+      .click({ force: true });
+
+    // 2. Click the Confirm button
+    cy.contains("Confirm")
+      .click();
+
+    // 3. Check the "Deleting" indicator shown
+    cy.contains("Deleting")
+
+    // 4. Check the "Deleting" indicator is gone
+    cy.get("Deleting").should("not.exist")
     
+    // 5. Check that the deleted appointment doesnt exist
+    cy.get('[data-testid="appointment"]')
+      .first()
+      .contains("Archie Cohen")
+      .should('not.exist')
+
   });
 });
