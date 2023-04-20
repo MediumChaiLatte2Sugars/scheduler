7ec73ce8d1a5501984e4b50811fecd1641558e48
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2023-04-20 16:24:59 -0400

    Refactor: improve code readability

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,4 +1,5 @@
 describe("Navigation", () => {
+
   it("should visit root", () => {
     cy.visit("/");
   });
@@ -10,39 +11,46 @@ describe("Navigation", () => {
       .should("have.class", "day-list__item--selected");
   });
 
-  xit("should book an interview", () => {
-    cy.visit("/")
-      // 2. Clicks on the "Add" button in the second appointment
-      .get(':nth-child(2) > .appointment__add > .appointment__add-button')
+  it("should book an interview", () => {
+    cy.visit("/");
+
+    cy.contains("Monday");
+
+    // 2. Clicks on the "Add" button in the second appointment
+    cy.get("[alt=Add]")
+      .first()
       .click()
       .get('[data-testid="student-name-input"]')
       // 3. Enters their name
       .type("Chic Ken", { delay: 60 })
       // 4. Chooses an interviewer
-      .get(':nth-child(1) > .interviewers__item-image')
-      .click()
-      // 5. Clicks the save button
-      .get('.button--confirm')
-      .click()
-      // 6. Sees the booked appointment
-      .get('[data-testid="appointment"]')
+      .get('[alt="Sylvia Palmer"]')
+      .click();
+
+    // 5. Clicks the save button
+    cy.contains('Save')
+      .click();
+
+    // 6. Sees the booked appointment
+    cy.get('[data-testid="appointment"]')
       .contains('Chic Ken');
   });
 
-  xit("should edit an interview", () => {
+  it("should edit an interview", () => {
     cy.visit("/")
       // 2. Clicks the edit button for the existing appointment
-      .get('.appointment__actions-button').first()
-      .invoke('show')
-      .click()
+      .get('.appointment__actions-button')
+      .first()
+      .click({ force: true })
       // 3. Changes the name and interviewer
       .get('[data-testid="student-name-input"]')
       .clear()
       .type("Chic Ken", { delay: 60 })
-      .get(':nth-child(2) > .interviewers__item-image')
+      .get('[alt="Tori Malcolm"]')
       .click()
-      // 4. Clicks the save button
-      .get('.button--confirm')
+
+    // 4. Clicks the save button
+    cy.contains('Save')
       .click()
       // 5. Sees the edit to the appointment
       .get('[data-testid="appointment"]')
@@ -51,16 +59,17 @@ describe("Navigation", () => {
 
   it("should cancel an interview", () => {
     cy.visit("/")
-       // 2. Clicks the delete button for the existing appointment
-      .get('.appointment__actions-button').eq(1)
-      .invoke('show')
-      .click()
-      // 3. Clicks the confirm button
-      .get('.appointment__actions > :nth-child(2)')
-      .click()
-      // 4. Sees that the appointment slot is empty
-      .get(':nth-child(1) > .appointment__add > .appointment__add-button')
-      .should('be.visible')
+      // 1. Clicks the delete button for the existing appointment
+      .get('.appointment__actions-button')
+      .eq(1)
+      .click({ force: true });
 
+    // 2. Clicks the confirm button
+    cy.contains('Confirm')
+      .click();
+
+    // 4. Sees that the appointment slot is empty
+    cy.get("[alt=Add]")
+      .should('be.visible');
   });
 });
